419f814cfe60f0c60c05b5cf9241c3a853c1529c
tokopediaSteven <benedictus.agre@tokopedia.com>
2018-10-27 22:19:28 +0700

    update

--- resources/views/payment/payments.blade.php
+++ resources/views/payment/payments.blade.php
@@ -1,14 +1,22 @@
-<table id="dataTable" class="table table-striped table-bordered" style="width:100%">
+<table id="dataTable2" class="table table-striped table-bordered" style="width:100%">
         <thead>
             <tr>
-                <th>Tenant</th>
                 <th>Payment Type</th>
                 <th>Date</th>
                 <th>Amount</th>
-                <th>Action</th>
+                <th>Notes</th>
             </tr>
         </thead>
-        <tbody>
         
+        <tbody>
+        @foreach($payments as $o)
+            <tr>
+                <td>{{$o->payment_type}}</td>
+                <td>{{$o->payment_date}}</td>
+                <td>{{$o->amount}}</td>
+                <td>{{$o->notes}}</td>
+            </tr>
+        @endforeach
         </tbody>
+        
 </table>
